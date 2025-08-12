4200cfe7a074307010112c8722fe5f36ff1cc3be
SaidurRahman1004 <saidurrahman1004@gmail.com>
2025-08-13 01:30:47 +0600

    commit 10

--- lib/Advanced Custom Card Design/EcommerceProductDetailsLayout.dart
+++ lib/Advanced Custom Card Design/EcommerceProductDetailsLayout.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 //Practice Task 6.29 ঃ Flutter Stack + Positioned – Social Media Feed Post Layout ans:
+/*
 class SocialMediaPost extends StatelessWidget {
    SocialMediaPost({super.key});
 
@@ -87,3 +88,136 @@ class SocialMediaPost extends StatelessWidget {
     );
   }
 }
+
+ */
+
+
+
+
+//Another
+import 'package:flutter/material.dart';
+
+class SocialMediaPost extends StatelessWidget {
+  SocialMediaPost({super.key});
+
+  var mycardItem = [
+    {
+      "img": "https://i.postimg.cc/jjKRZgRq/profile-pic-1.png",
+      "profileName": "Siyam Devo",
+      "post": "If you want to get respect, you have to give respect."
+    },
+    {
+      "img": "https://i.postimg.cc/tJnDcHjL/received-1211965183511363.jpg",
+      "profileName": "Hiru r Pola Natok rasel",
+      "post": "Naitokk"
+    },
+    {
+      "img": "https://i.postimg.cc/mDxzTffY/0.jpg",
+      "profileName": "Chris Brown",
+      "post": "Random caption text..."
+    },
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Social Media Post")),
+      body: ListView.builder(
+        itemCount: mycardItem.length,
+        itemBuilder: (context, index) {
+          return Card(
+            margin: EdgeInsets.all(8),
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10)),
+            elevation: 5,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                // Profile Info
+                ListTile(
+                  leading: CircleAvatar(
+                    backgroundImage:
+                    NetworkImage(mycardItem[index]["img"]!),
+                    radius: MediaQuery.of(context).size.width * 0.05,
+                  ),
+                  title: Text(
+                    mycardItem[index]['profileName']!,
+                    style: TextStyle(
+                      fontSize:
+                      MediaQuery.of(context).size.width * 0.04,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  subtitle: Text(
+                    "2 hours ago",
+                    style: TextStyle(
+                        fontSize:
+                        MediaQuery.of(context).size.width * 0.03),
+                  ),
+                  trailing: Icon(
+                    Icons.more_vert,
+                    size: MediaQuery.of(context).size.width * 0.04,
+                  ),
+                ),
+
+                // Caption
+                Padding(
+                  padding: EdgeInsets.all(
+                      MediaQuery.of(context).size.width * 0.03),
+                  child: Text(
+                    mycardItem[index]['post']!,
+                    style: TextStyle(
+                        fontSize:
+                        MediaQuery.of(context).size.width * 0.035),
+                  ),
+                ),
+
+                // Post Image + Like/Comment
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: Stack(
+                    children: [
+                      Image.network(
+                        mycardItem[index]["img"]!,
+                        fit: BoxFit.cover,
+                        height: MediaQuery.of(context).size.height * 0.3,
+                        width: double.infinity,
+                      ),
+                      Positioned(
+                        bottom: 10,
+                        left: 10,
+                        child: Row(
+                          children: [
+                            ElevatedButton.icon(
+                              onPressed: () {},
+                              icon: Icon(Icons.thumb_up),
+                              label: Text("Like"),
+                              style: ElevatedButton.styleFrom(
+                                  backgroundColor:
+                                  Colors.white.withOpacity(0.8)),
+                            ),
+                            SizedBox(width: 10),
+                            ElevatedButton.icon(
+                              onPressed: () {},
+                              icon: Icon(Icons.comment),
+                              label: Text("Comment"),
+                              style: ElevatedButton.styleFrom(
+                                  backgroundColor:
+                                  Colors.white.withOpacity(0.8)),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+
+
